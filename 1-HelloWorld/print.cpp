03456d63a7f01bb15228a89e4831972998cf5620
Paul McQuade <paulmcquad@gmail.com>
2024-07-10 17:31:57 +0100

    Update REQUIRE.md
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- 1-HelloWorld/print.cpp
+++ 1-HelloWorld/print.cpp
@@ -1,12 +1,12 @@
 // g++ -std=gnu++23  print.cpp -o print
 // Output:
-// Hello C++23!
+// Hello World C++23!
 
 
 #include <print>
  
 int main()
 {
-    std::print("{0} {2}{1}!\n", "Hello", 23, "C++"); // overload (2)
+    std::print("{0} {2}{1}!\n", "Hello World", 23, "C++"); // overload (2)
  
 }

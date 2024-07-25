1a4b9b5ea6449c89d0bf33141726b8a27d068457
Paul McQuade <paulmcquad@gmail.com>
2024-07-25 22:37:50 +0100

    3-Arrays
    remove old code
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- 3-Arrays/Arrays.md
+++ 3-Arrays/Arrays.md
@@ -15,11 +15,19 @@ There are 3 types of an array in C++ :
 A typical declaration for an array in C++ is:
 type name [elements];
 
+// local array Single-braces - Prior C++11
+
 int num[4] = {1, 2, 3, 4};
+double dnum[2] = {0.5, 1.5};
 
 OR
 
 #include <array>
 
 // num is an array of 4 int values
-std::array<int, 4> num{{1,2,3,4}}; // Double-braces required in C++11
+
+// Double-braces required in C++11
+
+std::array<int, 4> num{{1,2,3,4}};
+std::array<double, 2> dnum{{0.5, 1.5}};
+std::array<float, 3> fnum{{0.5, 1.5, 4.5}};
--- 3-Arrays/print_arr.cpp
+++ 3-Arrays/print_arr.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <array> // std::array
 #include <print>
 
 int main() {
-    // local array
+    // local array Single-braces - Prior C++11
     double scores[5] = {0.5, 1.5, 2.5, 3.5, 4.5};
+
+    // std::array
+    // Double-braces required in C++11
+    std::array<double, 5> newscores{{0.5, 1.5, 2.5, 3.5, 4.5}};
+    
+    std::array<float, 3> newfloat{{0.5, 1.5, 4.5}};
+
 
     for(int i{0}; i < 5; ++i) {
         std::print("{0} ", scores[i]);
     }
     std::print("\n");
+
+    for(int i{0}; i < 5; ++i) {
+        std::print("{0} ", newscores[i]);
+    }
+    std::print("\n");
+
+    for(int i{0}; i < 3; ++i) {
+        std::print("{0} ", newfloat[i]);
+    }
+    std::print("\n");
+
+
     return 0;
 }

2e561de9ca86001ee774c3a65cba5a2698df2953
Paul McQuade <paulmcquad@gmail.com>
2024-07-31 17:38:34 +0100

    4-Pointers
    use print file
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- 4-Pointers/pr_addr.cpp
+++ 4-Pointers/pr_addr.cpp
@@ -1,19 +1,24 @@
-#include <iostream>
 #include <stdlib.h>
-
-using namespace std;
+#include <print>
 
 int main()
 {
 
     int a{2}, b{3}, c{4};
-    int *pa = &a;
-    int *pb = &b;
-    int *pc = &c;
-    cout << "Value of pointer pa is: " << pa << endl;
-    cout << "Value of pointer pb is: " << pb << endl;
-    cout << "Value of pointer pc is: " << pc << endl;
-    cout << "The values of a, b, and c are: ";
-    cout << a << ", " << b << ", " << c << endl;
+    int* pa = &a;
+    int* pb = &b;
+    int* pc = &c;
+
+    //std::print("Value of pointer pa is: {0} \n", (void*)pa);
+
+    // Casting Operators in C++
+    // prefer static_cast because it's safer because it only lets you do "sensible" casts
+    std::print("Value of pointer pa is: {0} \n", static_cast<void*>(pa));
+    std::print("Value of pointer pa is: {0} \n", static_cast<void*>(pb));
+    std::print("Value of pointer pa is: {0} \n", static_cast<void*>(pc));
+
+    std::print("The values of a, b, and c are: ");
+    std::print("{0}, {1}, {2} \n",  a, b, c);
+    
     return 0;
 }
